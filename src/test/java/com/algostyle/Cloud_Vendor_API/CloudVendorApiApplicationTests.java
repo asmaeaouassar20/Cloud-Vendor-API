f295ed219816f2b264d0cc2da8155b2c6b1146d1
asmae <asmaeaouassar333@example.com>
2024-11-11 11:14:01 +0100

    create cloud vendor api

---
+++ src/test/java/com/algostyle/Cloud_Vendor_API/CloudVendorApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.algostyle.Cloud_Vendor_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudVendorApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
